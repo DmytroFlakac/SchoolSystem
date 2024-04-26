45d788fc612c2e5f21326fde378b596e9cf98ed6
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-04-26 14:28:08 +0200

    AppShell

--- SchoolSystem/SchoolSystem.App/Services/NavigationService.cs
+++ SchoolSystem/SchoolSystem.App/Services/NavigationService.cs
@@ -24,23 +24,23 @@ public class NavigationService : INavigationService
         where TViewModel : IViewModel
     {
         var route = GetRouteByViewModel<TViewModel>();
-        await Shell.Current.GoToAsync(route);
+        await Microsoft.Maui.Controls.Shell.Current.GoToAsync(route);
     }
     public async Task GoToAsync<TViewModel>(IDictionary<string, object?> parameters)
         where TViewModel : IViewModel
     {
         var route = GetRouteByViewModel<TViewModel>();
-        await Shell.Current.GoToAsync(route, parameters);
+        await Microsoft.Maui.Controls.Shell.Current.GoToAsync(route, parameters);
     }
 
     public async Task GoToAsync(string route)
-        => await Shell.Current.GoToAsync(route);
+        => await Microsoft.Maui.Controls.Shell.Current.GoToAsync(route);
 
     public async Task GoToAsync(string route, IDictionary<string, object?> parameters)
-        => await Shell.Current.GoToAsync(route, parameters);
+        => await Microsoft.Maui.Controls.Shell.Current.GoToAsync(route,parameters);
 
     public bool SendBackButtonPressed()
-        => Shell.Current.SendBackButtonPressed();
+        => Microsoft.Maui.Controls.Shell.Current.SendBackButtonPressed();
 
     private string GetRouteByViewModel<TViewModel>()
         where TViewModel : IViewModel 
--- SchoolSystem/SchoolSystem.App/Shell/AppShell.xaml.cs
+++ SchoolSystem/SchoolSystem.App/Shell/AppShell.xaml.cs
@@ -17,21 +17,22 @@ public partial class AppShell
     }
 
     // Example method to navigate to the student list view
-    private async void GoToStudentListView()
-    {
-        await _navigationService.GoToAsync<StudentListViewModel>();
-    }
+    [RelayCommand]
+    private async Task GoToStudentListView()
+        =>    await _navigationService.GoToAsync<StudentListViewModel>();
+    
+
 
     // Example method to navigate to the student detail view with a parameter
-    private async void GoToStudentDetailView(string studentId)
-    {
-        var parameters = new Dictionary<string, object?> { { "Id", studentId } };
-        await _navigationService.GoToAsync<StudentDetailViewModel>(parameters);
-    }
+    [RelayCommand]
+    private async Task GoToStudentDetailView(string studentId)
+    =>  await _navigationService.GoToAsync<StudentDetailViewModel>();
+  
 
+    [RelayCommand]
     // Example method to navigate to the student edit view
-    private async void GoToStudentEditView()
-    {
-        await _navigationService.GoToAsync<StudentEditViewModel>();
-    }
+    private async Task GoToStudentEditView()
+    => await _navigationService.GoToAsync<StudentEditViewModel>();
+    
+
 }
